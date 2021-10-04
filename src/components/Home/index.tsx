6a0c94a6f43768c8c47804beb1dd757b611a238c
devhijazi <heilhijzi@outlook.com>
2021-10-04 19:16:45 -0300

    re update

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,36 @@
+import { Container, Spacer } from './styles';
+import { DefaultOverlayContent } from '../DefaultOverlayContent';
+import { Wrapper } from '../Wrapper/Default';
+import { WrapperSection } from '../Wrapper/WrapperSection';
+
+import { UniqueOverlay } from '../Wrapper/UniqueOverlay';
+
+export const Home = (): JSX.Element => (
+  <Container>
+    <Wrapper>
+      <div>
+        {[
+          'Gabriel Hijazi',
+          'Sobre Mim',
+          'Meu Trabalho',
+          'Projetos',
+          'Contato',
+        ].map(wrapperName => (
+          <WrapperSection
+            key={wrapperName}
+            className="colored"
+            wrapperName={wrapperName}
+            overlayNode={
+              <DefaultOverlayContent
+                label={wrapperName}
+                description="Um dev maroto"
+              />
+            }
+          />
+        ))}
+      </div>
+      <Spacer />
+      <UniqueOverlay />
+    </Wrapper>
+  </Container>
+);
